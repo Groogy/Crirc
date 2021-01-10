c7d49508980509f3b9c5a6a6dac166fc778b6e8c
Groogy <groogy@groogy.se>
2021-01-10 14:17:16 +0100

    Added support for tags in a message

--- src/crirc/protocol/message.cr
+++ src/crirc/protocol/message.cr
@@ -16,22 +16,27 @@ class Crirc::Protocol::Message
   # The arguments as a string ("user1 +0", "pingmessage", ...)
   getter arguments : String?
 
+  # The tags as a string (tag-name-1=<tag-value-1>;tag-name-2=<tag-value-2>;)
+  getter tags : String?
+
   # The last argument when ":" ("This is a privmsg message", ...)
   getter message : String?
 
   R_SRC     = "(\\:(?<src>[^[:space:]]+) )"
   R_CMD     = "(?<cmd>[A-Z]+|\\d{3})"
   R_ARG_ONE = "(?:[^: ][^ ]*)"
+  R_TAG     = "(\\@(?<tag>#{R_ARG_ONE}(?: #{R_ARG_ONE})*) )"
   R_ARG     = "(?: (?<arg>#{R_ARG_ONE}(?: #{R_ARG_ONE})*))"
   R_MSG     = "(?: \\:(?<msg>.+)?)"
 
   def initialize(@raw)
-    m = raw.strip.match(/\A#{R_SRC}?#{R_CMD}#{R_ARG}?#{R_MSG}?\Z/)
+    m = raw.strip.match(/\A#{R_TAG}?#{R_SRC}?#{R_CMD}#{R_ARG}?#{R_MSG}?\Z/)
     raise ParsingError.new "The message (#{@raw}) is invalid" if m.nil?
     @source = m["src"]? || "0"
     @command = m["cmd"] # ? || raise InvalidMessage.new("No command to parse in \"#{raw}\"")
     @arguments = m["arg"]?
     @message = m["msg"]?
+    @tags = m["tag"]? 
   end
 
   # Concatenation of `arguments` and `message`.
@@ -57,6 +62,23 @@ class Crirc::Protocol::Message
     return (@arguments.as(String)).split(" ") if @message.nil?
     return [@message.as(String)] if @arguments.nil?
     return (@arguments.as(String)).split(" ") << (@message.as(String))
+  end
+
+  # The tags formated into an array
+  # If the message exists, it is preceded by ':'
+  #
+  # ```
+  # msg.raw_arguments # => "user1 +0 :do something"
+  # ```
+  def tags_list : Hash(String, String)
+    result = {} of String => String
+    if tags = @tags
+      tags.split(";") do |tag|
+        arr = tag.split("=")
+        result[arr[0]] = arr[1]
+      end
+    end
+    result
   end
 
   class ParsingError < Exception; end
